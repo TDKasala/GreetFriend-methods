94ba406bd3f36e162905b36f2efa3f10e334c5b0
Denis Kasala <deniskasala17@gmail.com>
2024-03-10 00:43:12 +0200

    Add project files.

---
+++ C# Masterclass/Program.cs
@@ -0,0 +1,27 @@
+﻿﻿using System;
+namespace HelloWorld
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Patrick";
+            string friend1 = "Noela";
+            string friend2 = "Raysun";
+
+            GreetFriend(friend);
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            Console.Read();
+        }
+
+
+        public static void GreetFriend(string friendName)
+        {
+
+            Console.WriteLine("Hi" + friendName + ", my friend");
+        }
+    }
+
+}
